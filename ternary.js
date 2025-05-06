d7036d75f9489a4f3cf31656415e1253272140f1
MohammedAqeel18 <aqeelworkmail@gmail.com>
2025-05-06 17:12:16 +0530

    update

---
+++ ternary.js
@@ -0,0 +1,12 @@
+let iitstudent = true;
+
+let nisalsir = iitstudent?"Apply for SE internship at IFS": "SORRY! you are not eligible for this internship";
+
+console.log(nisalsir);
+
+
+let score= 52;
+
+let grade= score>=90? "A": score>= 80 ?"B": score>70? "C+": score >=50? "C-": score>=40? "D+" : "YOU are FAILED";
+
+console.log(grade);
